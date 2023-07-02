8ca9925f5e431843b8e2261170d7a231e4400576
LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>
2023-07-02 23:06:41 +0530

    Update buttons.py

--- Music/helpers/buttons.py
+++ Music/helpers/buttons.py
@@ -204,15 +204,15 @@ class MakeButtons:
     def source_markup(self):
         buttons = [
             [
-                self.ikb(text="Github ❤️", url="https://github.com/The-HellBot"),
-                self.ikb(text="Repo 📦", url="https://github.com/The-HellBot/Music"),
+                self.ikb(text="Github ❤️", url="https://github.com"),
+                self.ikb(text="Repo 📦", url="https://github.com"),
             ],
             [
-                self.ikb(text="Under HellBot Network { 🇮🇳 }", url="https://t.me/HellBot_Networks"),
+                self.ikb(text="Under @DXinfo143 Network { 🇮🇳 }", url="https://t.me/DX_info"),
             ],
             [
-                self.ikb(text="Support 🎙️", url="https://t.me/HellBot_Chats"),
-                self.ikb(text="Updates 📣", url="https://t.me/Its_HellBot"),
+                self.ikb(text="Support 🎙️", url="https://t.me/DXinfo143"),
+                self.ikb(text="Updates 📣", url="https://t.me/DX_info"),
             ],
             [
                 self.ikb(text="🔙", callback_data="help|start"),
